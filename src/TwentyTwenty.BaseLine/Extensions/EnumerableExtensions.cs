4fc557b4eb66d8b11390b07bb5542d3affe9b92c
Eric Green <ericg@2020research.com>
2017-02-24 12:45:55 -0600

    simplified IsNullOrEmpty

--- src/TwentyTwenty.BaseLine/Extensions/EnumerableExtensions.cs
+++ src/TwentyTwenty.BaseLine/Extensions/EnumerableExtensions.cs
@@ -162,17 +162,7 @@ namespace System.Linq
 
         public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
         {
-            if (enumerable == null)
-            {
-                return true;
-            }
-
-            var collection = enumerable as ICollection<T>;
-            if (collection != null)
-            {
-                return collection.Count < 1;
-            }
-            return !enumerable.Any(); 
+            return enumerable == null || !enumerable.Any();
         }
     }
 }
