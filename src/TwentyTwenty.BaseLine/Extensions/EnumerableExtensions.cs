9f4612d280ef297d696104550fb3325853cb6758
Eric Green <edgeric@gmail.com>
2019-01-14 10:48:27 -0600

    Added list shuffle extension

--- src/TwentyTwenty.BaseLine/Extensions/EnumerableExtensions.cs
+++ src/TwentyTwenty.BaseLine/Extensions/EnumerableExtensions.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
+using System.Security.Cryptography;
 using TwentyTwenty.BaseLine;
 
 namespace System.Linq
@@ -234,6 +235,24 @@ namespace System.Linq
             while (start < end && string.IsNullOrWhiteSpace(list[start])) start++;
             while (end >= start && string.IsNullOrWhiteSpace(list[end])) end--;
             return list.Skip(start).Take(end - start + 1);
+        }
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            var provider = RandomNumberGenerator.Create();
+            var n = list.Count;
+
+            while (n > 1)
+            {
+                var box = new byte[1];
+                do provider.GetBytes(box);
+                while (!(box[0] < n * (Byte.MaxValue / n)));
+                var k = (box[0] % n);
+                n--;
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
         }
     }
 }
