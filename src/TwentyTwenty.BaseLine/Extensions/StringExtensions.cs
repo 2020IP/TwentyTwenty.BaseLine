6fd2923819a7170bc5dc031bd2b16f6c1e299e24
Eric Green <edgeric@gmail.com>
2021-03-18 07:59:56 -0500

    more string extension methods

--- src/TwentyTwenty.BaseLine/Extensions/StringExtensions.cs
+++ src/TwentyTwenty.BaseLine/Extensions/StringExtensions.cs
@@ -4,11 +4,24 @@ namespace TwentyTwenty.BaseLine
 {
     public static class StringExtensions
     {
-        public static string RemoveControlCharacters(this string input)
+        public static string RemoveBaseControlCharacters(this string input)
         {
             if (string.IsNullOrEmpty(input)) return input;
             
-            return Regex.Replace(input, @"\p{Cc}+", string.Empty);
+            return Regex.Replace(input, @"\p{Cc}+", string.Empty).Trim();
+        }
+        public static string RemoveAllControlCharacters(this string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            
+            return Regex.Replace(input, @"\p{C}+", string.Empty).Trim();
+        }
+
+        public static string RemoveNonAsciiCharacters(this string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+
+            return Regex.Replace(input, @"[^\u0000-\u007F]+", string.Empty).Trim();
         }
     }
 }
