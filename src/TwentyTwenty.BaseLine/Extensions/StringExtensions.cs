c86e1cf98ec9e583b764c323eb6407bd2e37e213
Scott Yeargain <syeargain@gmail.com>
2022-07-13 16:07:43 -0500

    addition sanitize extension method

--- src/TwentyTwenty.BaseLine/Extensions/StringExtensions.cs
+++ src/TwentyTwenty.BaseLine/Extensions/StringExtensions.cs
@@ -1,19 +1,23 @@
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace TwentyTwenty.BaseLine
 {
     public static class StringExtensions
     {
+        private static char[] removableChars = @"\{^}%`]""'>[~<#|*&$@=;/:+,?".ToCharArray();
+
         public static string RemoveBaseControlCharacters(this string input)
         {
             if (string.IsNullOrEmpty(input)) return input;
-            
+
             return Regex.Replace(input, @"\p{Cc}+", string.Empty).Trim();
         }
+
         public static string RemoveAllControlCharacters(this string input)
         {
             if (string.IsNullOrEmpty(input)) return input;
-            
+
             return Regex.Replace(input, @"\p{C}+", string.Empty).Trim();
         }
 
@@ -22,6 +26,17 @@ namespace TwentyTwenty.BaseLine
             if (string.IsNullOrEmpty(input)) return input;
 
             return Regex.Replace(input, @"[^\u0000-\u007F]+", string.Empty).Trim();
+        }
+
+        public static string Sanitize(this string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+
+            // TODO: Make removable chars a static char array that exists outside this method
+            //       var removableChars = @"\{^}%`]""'>[~<#|*&$@=;/:+,?";
+            return string.Concat(input.Where(c => !removableChars.Contains(c)))
+                .RemoveAllControlCharacters()
+                .RemoveNonAsciiCharacters();
         }
     }
 }
